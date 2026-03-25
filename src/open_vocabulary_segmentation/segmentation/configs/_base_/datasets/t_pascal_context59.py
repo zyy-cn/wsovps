1de2946160385b8632e391a4cc103bb7cb2b6c46
zyy-cn <zacchues@qq.com>
2026-03-25 19:36:36 +0800

    Restore README-faithful VOC/Context dataset config semantics

--- src/open_vocabulary_segmentation/segmentation/configs/_base_/datasets/t_pascal_context59.py
+++ src/open_vocabulary_segmentation/segmentation/configs/_base_/datasets/t_pascal_context59.py
@@ -1,7 +1,7 @@
 _base_ = ["../custom_import.py"]
 # dataset settings
 dataset_type = "PascalContextDataset59"
-data_root = "./data/pascal_context/VOCdevkit/VOC2010"
+data_root = "./data/VOCdevkit/VOC2010"
 img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
 test_pipeline = [
     dict(type="LoadImageFromFile"),
@@ -24,8 +24,8 @@ data = dict(
         type=dataset_type,
         data_root=data_root,
         img_dir="JPEGImages",
-        ann_dir="SegmentationClass",
-        split="ImageSets/Segmentation/train.txt",
+        ann_dir="SegmentationClassContext",
+        split="ImageSets/SegmentationContext/train.txt",
         pipeline=test_pipeline,
     )
 )
